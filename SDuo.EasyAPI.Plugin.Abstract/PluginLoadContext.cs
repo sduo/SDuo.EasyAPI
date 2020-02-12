458a34bfb46f3a99ce87d52e9c2b4b7823dbc3be
SDuo <sduo@vip.qq.com>
2020-02-12 22:08:22 +0800

    支持加载插件依赖

---
+++ SDuo.EasyAPI.Plugin.Abstract/PluginLoadContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace SDuo.EasyAPI.Plugin.Abstract
+{
+    public class PluginLoadContext : AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver resolver { get; set; }
+
+        public PluginLoadContext(string plugin) :base(true)
+        {
+            resolver = new AssemblyDependencyResolver(plugin);
+        }
+
+        protected override Assembly Load(AssemblyName name)
+        {
+            string path = resolver.ResolveAssemblyToPath(name);
+            if (path != null)
+            {
+                Console.WriteLine($"Loading assembly {path} into the PluginAssemblyLoadContext");
+                return LoadFromAssemblyPath(path);
+            }
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string name)
+        {
+            string path = resolver.ResolveUnmanagedDllToPath(name);
+            if (path != null)
+            {
+                Console.WriteLine($"Loading assembly {path} into the HostAssemblyLoadContext");
+                return LoadUnmanagedDllFromPath(path);
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
