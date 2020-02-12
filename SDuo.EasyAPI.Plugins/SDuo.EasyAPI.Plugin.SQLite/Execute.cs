e3aec796ec5e9f6653eb5d4c81e6d093f1378f59
SDuo <sduo@vip.qq.com>
2020-02-12 12:56:07 +0800

    实现插件 SDuo.EasyAPI.Plugin.SQLite 的 Execute

--- SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.SQLite/Execute.cs
+++ SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.SQLite/Execute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.SQLite;
+using System.Net;
 using System.Threading.Tasks;
 using System.Xml;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using SDuo.EasyAPI.Plugin.Abstract;
 
@@ -10,11 +12,61 @@ namespace SDuo.EasyAPI.Plugin.SQLite
 {
     public class Execute : IPlugin
     {
-        public string Version => throw new NotImplementedException();
+        public string Version => "0.0.1";
 
-        public Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback)
+        public async Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback)
         {
-            throw new NotImplementedException();
+            if (context == null)
+            {
+                throw new NullReferenceException(nameof(context));
+            }
+
+            if (action == null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(action));
+                return 1;
+            }
+
+            string datasource = action.Attributes[nameof(datasource)].Value;
+            if (string.IsNullOrEmpty(datasource))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(datasource));
+                return 2;
+            }
+
+            string sql = action.SelectSingleNode(nameof(sql))?.InnerText;
+            if (string.IsNullOrEmpty(sql))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(sql));
+                return 3;
+            }
+
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            string value = null;
+            foreach (string key in context.Request.Form.Keys)
+            {
+                if (param.ContainsKey(key))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, $"{nameof(param)}:{key}");
+                    return 4;
+                }
+                value = WebUtility.UrlDecode(context.Request.Form[key]);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    param.Add(key, value);
+                }
+            }
+
+            using (SQLiteConnection connection = new SQLiteConnection($"data source={datasource}"))
+            {
+                data = await connection.ExecuteAsync(sql, param).ConfigureAwait(false);
+                callback?.Invoke(data);
+                return 0;
+            }
         }
     }
 }
