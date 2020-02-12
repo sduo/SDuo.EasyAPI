762fb44db0d8b9be1088d5448f36682062fc9de6
SDuo <sduo@vip.qq.com>
2020-02-12 12:44:57 +0800

    定义接口

---
+++ SDuo.EasyAPI.Plugin.Abstract/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+
+namespace SDuo.EasyAPI.Plugin.Abstract
+{
+    public interface IPlugin
+    {
+        public const string NameSpace = "SDUO.EasyAPI.Plugin";
+        public const string X_PLUGIN_MESSAGE = "X-Plugin-Message";
+
+        string Version { get; }
+        Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback);
+    }
+}
