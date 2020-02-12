d497abfe7d72a80e9c4b352171dd2d7b5d928bd3
SDuo <sduo@vip.qq.com>
2020-02-12 12:49:44 +0800

    添加 SDuo.EasyAPI.Plugin.Abstract 引用

---
+++ SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.SQLite/Execute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using SDuo.EasyAPI.Plugin.Abstract;
+
+namespace SDuo.EasyAPI.Plugin.SQLite
+{
+    public class Execute : IPlugin
+    {
+        public string Version => throw new NotImplementedException();
+
+        public Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
