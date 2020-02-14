21ae538dcb7733940b74fe85742d9b1a5a0c273a
SDuo <sduo@vip.qq.com>
2020-02-14 16:40:33 +0800

    添加 SDuo.EasyAPI.Plugin.Nonce.Memory 插件

---
+++ SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.Nonce.Memory/Memory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using SDuo.EasyAPI.Plugin.Abstract;
+
+namespace SDuo.EasyAPI.Plugin.Nonce
+{
+    public class Memory : IPlugin
+    {
+        public string Version => "0.0.1";
+
+        public async Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
