d23dab42ff85e7524c505b4db4cf7ffd06282ab8
SDuo <sduo@vip.qq.com>
2020-02-14 18:21:17 +0800

    实现 SDuo.EasyAPI.Plugin.Nonce.Memory 插件

--- SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.Nonce.Memory/Memory.cs
+++ SDuo.EasyAPI.Plugins/SDuo.EasyAPI.Plugin.Nonce.Memory/Memory.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using System.Xml;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 using SDuo.EasyAPI.Plugin.Abstract;
 
 namespace SDuo.EasyAPI.Plugin.Nonce
@@ -12,7 +15,63 @@ namespace SDuo.EasyAPI.Plugin.Nonce
 
         public async Task<int> ExecuteAsync(HttpContext context, XmlNode action, object data, Action<object> callback)
         {
-            throw new NotImplementedException();
+            if (context == null)
+            {
+                throw new NullReferenceException(nameof(context));
+            }
+
+            if (action == null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(action));
+                return 1;
+            }
+
+            string authcode = data as string;
+
+            if (string.IsNullOrEmpty(authcode))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(authcode));
+                return 2;
+            }
+
+            string nonce = context.Request.Headers[nameof(nonce)];
+
+            if (string.IsNullOrEmpty(nonce))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(authcode));
+                return 3;
+            }
+
+            using (MemoryCache cache = new MemoryCache(Options.Create(new MemoryCacheOptions())))
+            {
+                string key = $"Nonce:{authcode}";
+
+                string prefix = action.Attributes[nameof(prefix)]?.Value;
+                if (!string.IsNullOrEmpty(prefix))
+                {
+                    key = string.Join(":", prefix, key);
+                }
+                string value = cache.Get(key) as string;
+                if (string.Equals(nonce, value, StringComparison.OrdinalIgnoreCase))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    context.Response.Headers.Add(IPlugin.X_PLUGIN_MESSAGE, nameof(authcode));
+                    return 4;
+                }
+                else
+                {
+                    string  timeout  = action.Attributes[nameof(timeout)]?.Value;
+                    if (!long.TryParse(timeout, out long ms))
+                    {
+                        ms = 5000 ;
+                    }
+                    cache.Set(key, value, TimeSpan.FromTicks(ms * TimeSpan.TicksPerMillisecond));
+                }                
+            }
+            return 0;
         }
     }
 }
